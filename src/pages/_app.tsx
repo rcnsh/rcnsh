101fbe6a9be635dea603ae72785a036f95b9ffd5
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-11-08 18:54:35 +0000

    trying out mono

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { Analytics } from "@vercel/analytics/react";
-import { GeistSans } from "geist/font";
+import { GeistMono } from "geist/font";
 
 import "@/styles/globals.css";
 import Footer from "@/components/footer";
@@ -9,7 +9,7 @@ import Layout from "@/pages/Layout";
 const App: AppType = ({ Component, pageProps }) => {
   return (
     <div
-      className={`m-auto w-[80%] bg-[#171717] text-center font-mono text-[#cdc8c2] md:w-[50%] md:text-left ${GeistSans.className}`}
+      className={`m-auto w-[80%] bg-[#171717] text-center font-mono text-[#cdc8c2] md:w-[50%] md:text-left ${GeistMono.className}`}
     >
       <Layout>
         <Component {...pageProps} />
