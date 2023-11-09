7b66ce5ae75e2bee33e129d15a2234913fff5e74
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-11-09 18:14:42 +0000

    remove dialog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { Analytics } from "@vercel/analytics/react";
 import { GeistMono } from "geist/font";
-import { Dialog, DialogContent, DialogTitle } from "@/components/dialog";
 import { useKonami } from "react-konami-code";
 import { clsx } from "clsx";
 
@@ -12,10 +11,8 @@ import { FaGithub } from "react-icons/fa";
 import React, { useState } from "react";
 
 const App: AppType = ({ Component, pageProps }) => {
-  const [dialogOpen, setDialogOpen] = useState(false);
   const [partyMode, setPartyMode] = useState(false);
   useKonami(() => {
-    setDialogOpen(true);
     setPartyMode(true);
   });
 
@@ -37,11 +34,6 @@ const App: AppType = ({ Component, pageProps }) => {
           <Layout>
             <Component {...pageProps} />
           </Layout>
-          <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-            <DialogContent className={"flex justify-center"}>
-              <DialogTitle>You found me! Congrats?</DialogTitle>
-            </DialogContent>
-          </Dialog>
           <hr className="my-4 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25" />
           <footer className={"flex justify-evenly"}>
             <span className={"font-bold text-[#cdc8c2]"}>Jacob Wiltshire</span>
